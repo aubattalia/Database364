fbdff673fcb9b3171ab5e67b9fb76d34100b61b9
aubattalia <sneaksfile@gmail.com>
2019-12-02 18:07:13 -0500

    Update InsertStatementsInfo364.sql

--- InsertStatementsInfo364.sql
+++ InsertStatementsInfo364.sql
@@ -2,15 +2,17 @@
 --[char(25) not null, date not null, char(25) not null, char(25) not null, char(25) not null, char(25) not null]
 insert into
 PURCHASE (PurchNo, PurchDate, PmtID, ProductID, CustomerID, EmpID)
-values(1001, 26.10.16, 90893, 875684, 10294853, 11112)
+values(1001, date '2008-11-08', 90893, 875684, 10294853, 11112)
 
 --Customer Customer [CustID, CustFirst, CustLast, Address, CustPhone, CustEmail]
 --[char(25) not null, 
 
 
---RETURN [ReturnID, returnReason, returnDate, ProductID(FK)]
---[char(25) not null, char(25), Date not null, char(25) not null]
-
+--RETURN [ReturnID, ProductID, returnReason, returnDate]
+--[char(25) not null,char(25), char(25), Date not null]
+insert into
+RETURN(ReturnID, ProductID, returnReason, returnDate)
+values(890438, 875684, 'My dog ate my sock', date '2008-11-10')
 
 
 --Payment Payment [PmtID, pmtAmount, pmtType, pmtChargedDate]
@@ -34,6 +36,7 @@ values(1001, 26.10.16, 90893, 875684, 10294853, 11112)
 
 
 --Product [ProductID, ProductType, ProductName]
+--[NUMBER(11,0), 
 
 --Product_Women
 
@@ -44,7 +47,10 @@ values(1001, 26.10.16, 90893, 875684, 10294853, 11112)
 
 
 --STORE  [StoreID, StoreName, StoreAddress]
-
+--[CHAR(25), VARCHAR2(255), VARCHAR2(100)]
+INSERT INTO
+STORE(StoreID, StoreName, StoreAddress)
+values('1000001', 'Recursion Retail', '1800 W Number St, 23409, Richmond, VA')
 
 
 --Employee [empID, empFirst, empLast]
@@ -55,7 +61,11 @@ values(1001, 26.10.16, 90893, 875684, 10294853, 11112)
 
 
 
---Supplier [SupplierID, SuplierName, SupplierEmail]
+--Supplier [SupplierID, SupplierName, SupplierEmail]
+--[Number, VARCHAR2(25), VARCHAR2(255)]
+INSERT INTO 
+SUPPLIER(SupplierID, SupplierName, SupplierEmail)
+values(3, 'Buy4Cheap', 'ibuythischeap@hotmail.com')
 
 
 --Transportation[TransportID, transportType]
