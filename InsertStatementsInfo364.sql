7a499e7128f82cf34f98eb7b986437dce76fff7c
aubattalia <sneaksfile@gmail.com>
2019-12-04 18:30:23 -0500

    Update InsertStatementsInfo364.sql

--- InsertStatementsInfo364.sql
+++ InsertStatementsInfo364.sql
@@ -13,8 +13,8 @@ CUSTOMER values('10294853', 'Jimminy', 'Cricket', '780 Mellow Dr., Richmond VA, 
 --RETURN [ReturnID, ProductID, returnReason, returnDate]
 --[char(25) not null,char(25), char(25), Date not null]
 insert into
-RETURN(ReturnID, ProductID, returnReason, returnDate)
-values(890438, '78457', 'My dog ate my sock', date '2008-11-10')
+RETURN
+values('890438', '78457', 'My dog ate my sock', date '2008-11-10')
 
 
 --Payment [PmtID, pmtAmount, pmtType, pmtChargedDate]
@@ -42,41 +42,41 @@ values('78458', 'Kids', 'Tshirt')
 --STORE  [StoreID, StoreName, StoreAddress]
 --[CHAR(25), VARCHAR2(255), VARCHAR2(100)]
 INSERT INTO
-STORE(StoreID, StoreName, StoreAddress)
+STORE
 values('1000001', 'Recursion Retail', '1800 W Number St, 23409, Richmond, VA')
 
 
 --Employee [empID, empFirst, empLast]
 --[int, varchar2(25), varchar2(25)]
 INSERT INTO 
-EMPLOYEE(empID, empFirst, empLast)
-values(11112, Harris, Teeter)
+EMPLOYEE
+values('11112', 'Harris', 'Teeter')
 
 
 --Warehouse [WarehouseID, ProductID (FK), Capacity]
 --[CHAR(25), CHAR(25), VARCHAR2(25)]
 INSERT INTO
-WAREHOUSE(WarehouseID, ProductID, Capacity)
- values(9001, 875684, 4000000)
+WAREHOUSE
+ values('9001', '875684', '4000000')
 
 
 --Supplier [SupplierID, SupplierName, SupplierEmail]
 --[Number, VARCHAR2(25), VARCHAR2(255)]
 INSERT INTO 
-SUPPLIER(SupplierID, SupplierName, SupplierEmail)
+SUPPLIER
 values(3, 'Buy4Cheap', 'ibuythischeap@hotmail.com')
 
 
 --Transportation[TransportID, transportType]
 --[CHAR(25), CHAR(25)]
 INSERT INTO
-TRANSPORTATION(TransportID, transportType)
-values(675843, truck)
+TRANSPORTATION
+values('675843', 'truck')
 
 
 --Order[OrderID, WarehouseID(FK), ProductID(FK), SupplierID(FK)]
 --[CHAR(25), CHAR(25), CHAR(25), CHAR(25)]
 INSERT INTO 
 ORDER(OrderID, WarehouseID, ProductID, Suppl)
-values(322228, 9001, 78457, 3)
+values('322228', '9001', '78457', 3)
 
